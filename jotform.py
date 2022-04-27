8e681995ec092812c6cf58538b24ae1d95c6c9b8
babakshirvani <bk.shirvani@gmail.com>
2022-04-27 14:20:38 -0700

    fix forbidden 403 error

--- jotform.py
+++ jotform.py
@@ -55,7 +55,13 @@ class JotformAPIClient:
             self._log(params)
 
         headers = {
-            'apiKey': self.__apiKey
+            'apiKey': self.__apiKey,
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive',``
         }
 
         if (method == 'GET'):
